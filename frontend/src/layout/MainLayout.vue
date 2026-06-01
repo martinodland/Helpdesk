c70cf9e155d8dd125a2782c71ea47be30821a3c1
martinodland <martin.odland@hjelseth.com>
2026-06-01 14:33:02 +0200

    Initial Commit

---
+++ frontend/src/layout/MainLayout.vue
@@ -0,0 +1,13 @@
+<script setup>
+import Footer from '@/components/Footer.vue';
+import Header from '@/components/Header.vue';
+
+</script>
+
+<template>
+    <div>
+        <Header />
+            <slot />
+        <Footer />
+    </div>
+</template>
---
+++ frontend/src/pages/Home.vue
@@ -0,0 +1,10 @@
+<script setup>
+import MainLayout from '../layout/MainLayout.vue';
+
+</script>
+
+<template>
+  <MainLayout>
+    <div class="bg-red-500 text-white p-4">Tailwind test</div>
+  </MainLayout>
+</template>
